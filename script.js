9b592b4cd22005a4c356081a41314f0a36316de5
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-01-29 14:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-
+    // Prevent the default form submission
+    event.preventDefault();
+    
+    // Get the form inputs by name
+    let firstName = document.getElementsByName('fname')[0].value;
+    let lastName = document.getElementsByName('lname')[0].value;
+    
+    // Alert the full name
+    alert(firstName + " " + lastName);
 }
